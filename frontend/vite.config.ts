0b538a919e61fc45e0c524a94af94ead0554101f
Divyanshi Dixit <divyanshi259dixit@gmail.com>
2026-01-07 15:03:32 -0800

    Fixed build issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
 });
